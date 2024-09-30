7bf59519e877b152755ac07deb5975376cd5c490
ravikr126 <ravi14577@gmail.com>
2024-09-30 11:45:18 +0530

    home section

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="pb-20 pt-36">
+        <h1 className="text-white">hii</h1>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
