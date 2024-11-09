c4a943117b4516a5a3afa4aebb12f7801533ccec
ravikr126 <ravi14577@gmail.com>
2024-11-09 16:45:02 +0530

    nav data dynamically done

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,20 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingDock } from "@/components/ui/FloatingDock";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-   <main  className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    <div className="max-w-7xl w-full">
-    <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome/>}]} />
-     <Hero/>
-     <Grid/>
-     {/* <FloatingDock /> */}
-     <RecentProjects/>
-    </div>
-   </main>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        {/* <FloatingDock /> */}
+        <RecentProjects />
+      </div>
+    </main>
   );
 }
