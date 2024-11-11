cb25ab97057ba979e053fab842cee25e33c5374c
ravikr126 <ravi14577@gmail.com>
2024-11-12 00:13:47 +0530

    testimonial complete

---
+++ components/Clients.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { companies, testimonials } from "@/data";
+const Clients = () => {
+  return (
+    <section id="testimonials" className="py-20">
+      <h1 className="heading">
+        Kind words from
+        <span className="text-purple"> satisfied clients</span>
+      </h1>
+
+      <div className="flex flex-col items-center mt-5 max-lg:mt-10">
+      
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+     
+    </section>
+  );
+};
+
+export default Clients;
