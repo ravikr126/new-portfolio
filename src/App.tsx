ab717dc7c90f6b0b1eb27873902125728d6aa088
Ravi Kumar <ravi@finnspire.com>
2025-05-09 13:52:38 +0530

    new init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { navItems } from "./assets/data";
+function App() {
+  
+
+  return (
+    <>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-full">
+        
+      hii
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
