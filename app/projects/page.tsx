9dffea528381eb1e3ccc1316563b66bd2dee8ed8
ravikr126 <ravi14577@gmail.com>
2025-05-21 14:40:02 +0530

    fix

--- app/projects/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import { HeroParallax } from "../../components/ui/HeroParallax";
-import {products} from '@/data'
-export default function ProjectPage() {
-  return <HeroParallax products={products} />;
-}
